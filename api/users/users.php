6ee7574943ba791af89d40282665fbe8546926a3
PedroSousa2020 <sousapedro747@gmail.com>
2021-05-25 03:09:07 +0100

    User read controller done

--- api/users/users.php
+++ api/users/users.php
@@ -1,30 +1,81 @@
 <?php
+
     header("Content-Type: application/json");
 
+    if(isset($_GET["id"]))
+        getOne($_GET["id"]);
+    else
+        getAll();
+
+   function getAll(){
+
     include ("../connection.php");
+    
+    $data = array(
+        "data" => array()
+    );
 
     try {
-
         $stmt = $pdo->query("SELECT * FROM user");
-        $data = array();
-        $i = 0;
-
+       
         while($user = $stmt->fetch(PDO::FETCH_ASSOC)){
-              $data[$i]["id"] = $user["id"];
-              $data[$i]["name"] = $user["name"];
-              $data[$i]["description"] = $user["description"];
-              $data[$i]["profission"] = $user["profission"];
-              $data[$i]["email"] = $user["email"];
-              $data[$i]["city"] = $user["city"];
-              $data[$i]["country"] = $user["country"];
-              $data[$i]["telephone"] = $user["telephone"];
-              $data[$i]["code"] = $user["code"];
-              $i++; 
-        }
+              extract($user);
 
+              $datum = array(
+                  "id" => $user["id"],
+                  "name" => $user["name"],
+                  "description" => $user["description"],
+                  "profission" => $user["profission"],
+                  "email" => $user["email"],
+                  "city" => $user["city"],
+                  "country" => $user["country"],
+                  "telephone" => $user["telephone"],
+                  "code" => $user["postal_code"] 
+              );
+
+              array_push($data['data'], $datum);
+        }
+        
         echo json_encode($data, JSON_PRETTY_PRINT);
 
     } catch (PDOException $ex) {
         var_dump($ex);
     }
-?>
+   }
+
+   function getOne($id){
+
+    include ("../connection.php");
+    
+    $data = array(
+        "data" => array()
+    );
+
+    try {
+        $stmt = $pdo->query("SELECT * FROM user where id = ".$id);
+       
+        while($user = $stmt->fetch(PDO::FETCH_ASSOC)){
+              extract($user);
+
+              $datum = array(
+                  "id" => $user["id"],
+                  "name" => $user["name"],
+                  "description" => $user["description"],
+                  "profission" => $user["profission"],
+                  "email" => $user["email"],
+                  "city" => $user["city"],
+                  "country" => $user["country"],
+                  "telephone" => $user["telephone"],
+                  "code" => $user["postal_code"] 
+              );
+
+              array_push($data['data'], $datum);
+        }
+        
+        echo json_encode($data, JSON_PRETTY_PRINT);
+
+    } catch (PDOException $ex) {
+        var_dump($ex);
+    }
+
+   }
